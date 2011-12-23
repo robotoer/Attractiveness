809d0adb0913bbdcec21a073107bf46222e382de
Robert Chu <allegrormc@gmail.com>
2011-12-23 04:48:25 -0800

    Cleaned up code & commented things This is the commit before adding datastore stuff

--- src/com/appspot/attractiveness/client/Attractiveness.java
+++ src/com/appspot/attractiveness/client/Attractiveness.java
@@ -7,6 +7,7 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -15,9 +16,12 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Attractiveness implements EntryPoint {
+	// Some semblance of organized layout.  This should probably be in static code eventually
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private HorizontalPanel submitPanel = new HorizontalPanel();
 	private VerticalPanel ratePanel = new VerticalPanel();
+	
+	// The dynamic elements! (kinda)
 	private RadioButton rate1 = new RadioButton("rating", "1");
 	private RadioButton rate2 = new RadioButton("rating", "2");
 	private RadioButton rate3 = new RadioButton("rating", "3");
@@ -27,38 +31,49 @@ public class Attractiveness implements EntryPoint {
 	private Image portrait = new Image("http://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/600px-Smiley.svg.png");
 
 	/**
-	 * This is the entry point method.
+	 * This is the entry point method of our application.
+	 * This method should do the following:
+	 * <ul>
+	 * <li>Gather and store the rater's facebook information
+	 * <li>Construct the UI</li>
+	 * </ul>
 	 */
 	public void onModuleLoad() {
+		// Gather & store user's facebook information
+		// TODO: actually do this
+		
+		// Build the UI.  Hooray for in code UI building!
 		ratePanel.add(rate1);
 		ratePanel.add(rate2);
 		ratePanel.add(rate3);
 		ratePanel.add(rate4);
 		ratePanel.add(rate5);
-
 		submitPanel.add(ratePanel);
 		submitPanel.add(submitBtn);
-
 		mainPanel.add(portrait);
 		mainPanel.add(submitPanel);
-
 		RootPanel.get("contentDiv").add(mainPanel);
 
+		// Add the click handler.  This should:
+		//  - Save the labeled image to the datastore
+		//  - Load the next image and reset the UI
 		submitBtn.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				String selected = "unselected";
+				// Get the rating
+				int rating = 0;
 				if (rate1.getValue())
-					selected = "1";
+					rating = 1;
 				else if (rate2.getValue())
-					selected = "2";
+					rating = 2;
 				else if (rate3.getValue())
-					selected = "3";
+					rating = 3;
 				else if (rate4.getValue())
-					selected = "4";
+					rating = 4;
 				else if (rate5.getValue())
-					selected = "5";
-				Window.alert("The rating selected: " + selected);
+					rating = 5;
+				
+				// Save the rating to the datastore
 			}
 		});
 	}
