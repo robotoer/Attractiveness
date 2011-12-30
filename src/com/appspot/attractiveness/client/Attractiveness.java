77ea3b4e010530d0d1fd2e5e6b4f319a6f0a2e06
Robert Chu <allegrormc@gmail.com>
2011-12-30 01:21:43 -0800

    Removed unnecessary import in Attractiveness.java

--- src/com/appspot/attractiveness/client/Attractiveness.java
+++ src/com/appspot/attractiveness/client/Attractiveness.java
@@ -1,7 +1,6 @@
 package com.appspot.attractiveness.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
